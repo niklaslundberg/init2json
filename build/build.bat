36fd4022fe386f36ea7dd744fe2f875799b1e20c
Niklas Lundberg <info@nlundberg.se>
2023-08-23 11:14:01 +0200

    Add build bootstrapper.

---
+++ build/build.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+dotnet --info
+SET Arbor.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.Build.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.Build.NuGet.Package.Artifacts.Suffix=
+SET Arbor.Build.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.Build.NuGetPackageVersion=
+SET Arbor.Build.Vcs.Branch.Name.Version.OverrideEnabled=true
+SET Arbor.Build.Vcs.Branch.Name=%GITHUB_REF%
+SET Arbor.Build.VariableOverrideEnabled=true
+SET Arbor.Build.Artifacts.CleanupBeforeBuildEnabled=true
+SET Arbor.Build.NetAssembly.Configuration=
+SET Arbor.Build.MSBuild.NuGetRestore.Enabled=true
+SET Arbor.Build.Tools.External.Xunit.NetCoreApp.Enabled=false
+SET Arbor.Build.BuildNumber.UnixEpochSecondsEnabled=true
+SET Arbor.Build.Tools.External.SymbolServer.Enabled=true
+SET Arbor.Build.Tools.External.VisualStudio.Version.PreRelease.Enabled=true
+SET Arbor.Build.Tools.External.MSBuild.AllowPrerelease.Enabled=true
+SET Arbor.Build.Log.Level=Debug
+SET Arbor.Build.Tools.External.MSBuild.DotNet.Enabled=true
+SET Arbor.Build.Vcs.Branch.BranchModel=GitFlowBuildOnMain
+
+SET Fallback.Version.Build=0
+
+IF "%Arbor.Build.Bootstrapper.AllowPrerelease%" == "" (
+	SET Arbor.Build.Bootstrapper.AllowPrerelease=true
+)
+
+SET Arbor.Build.NuGet.ReinstallArborPackageEnabled=true
+SET Arbor.Build.NuGet.VersionUpdateEnabled=false
+SET Arbor.Build.Artifacts.PdbArtifacts.Enabled=true
+SET Arbor.Build.NuGet.Package.CreateNuGetWebPackages.Enabled=true
+CALL dotnet arbor-build
+
+EXIT /B %ERRORLEVEL%
